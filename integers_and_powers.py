e6d3f24fe2603edc6a09fa53fbbfc6cafa2788ff
CMac <chris.mac.murray@offerpop.com>
2014-08-22 12:20:24 -0400

    adding more exercises, some not done

--- integers_and_powers.py
+++ integers_and_powers.py
@@ -5,4 +5,18 @@ entered byu the user. If no such pair of integers exist, it should print a
 message to that effect
 """
 
+#Take an integer from the user
 
+num = int(raw_input('please input an integer '))
+root = 0
+pwr = 0
+
+while root ** pwr != abs(num):
+    root = root +1
+    while root ** pwr != abs(num):
+        pwr = pwr +1
+        if pwr == 6 or root ** pwr == num:
+            break
+
+if root ** pwr != num:
+    print 'No such integer pair exist'
